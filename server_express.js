5c8a721097f2ff4517febaa4197526b77ca920b3
evan20010126 <55246608+evan20010126@users.noreply.github.com>
2022-10-07 23:45:28 +0800

    Update server_express.js

--- server_express.js
+++ server_express.js
@@ -7,3 +7,13 @@ app.use(express.static(__dirname));
 app.listen(port, () => {
     console.log('Express is running on http://localhost:' + port.toString());
 });
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', ws => {
+    console.log('Client connected');
+    
+    ws.on('close', () => {
+        console.log('Close connected')
+    })
+})
